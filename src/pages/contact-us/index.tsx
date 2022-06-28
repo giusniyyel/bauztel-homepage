cb1792dd424e99442461daa15e72867e950a0f59
Daniel Campos <37324092+giusniyyel@users.noreply.github.com>
2022-06-28 11:07:10 -0500

    :sparkles: feat: add google recaptcha to contact form

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Center,
   Heading,
   Stack,
   VStack,
@@ -13,10 +14,15 @@ import {
 import { Layout } from "../../components/Layout";
 import { Hero } from "../../components/Hero";
 import { Chakra } from "../../../Chakra";
+import ReCAPTCHA from "react-google-recaptcha";
 
 interface IndexProps {
   cookies?: string;
 }
+
+const onSuccessCaptcha= () => {
+  console.log("Hubo un cambio");
+};
 
 const ContactUsPage = ({ cookies }: IndexProps) => (
   <Chakra cookies={cookies}>
@@ -35,13 +41,13 @@ const ContactUsPage = ({ cookies }: IndexProps) => (
           p={8}
         >
           <Stack direction={{ base: "column" }} spacing={2} mb={8}>
-              <Heading>Contáctanos</Heading>
-              <Text>
-                En <b>Bauztel Café</b> queremos conocer tus opiniones,
-                comentarios, o el interés que tienes sobre nuestros productos,
-                así que te invitamos a dejar tu información para que podamos contactarte a
-                la brevedad.
-              </Text>
+            <Heading textAlign="center">Contáctanos</Heading>
+            <Text>
+              En <b>Bauztel Café</b> queremos conocer tus opiniones,
+              comentarios, o el interés que tienes sobre nuestros productos, así
+              que te invitamos a dejar tu información para que podamos
+              contactarte a la brevedad.
+            </Text>
           </Stack>
           <Stack
             justifyContent="space-between"
@@ -62,10 +68,17 @@ const ContactUsPage = ({ cookies }: IndexProps) => (
                   <Textarea
                     placeholder="Estoy interesado en conocer acerca de..."
                     id="message"
-                    size="md"
+                    size="lg"
+                    minH={32}
                     resize="none"
                   />
                 </FormControl>
+                <Center>
+                  <ReCAPTCHA
+                    sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                    onChange={onSuccessCaptcha}
+                  />
+                </Center>
                 <Button
                   w="100%"
                   backgroundColor="#1E3760"
@@ -76,10 +89,6 @@ const ContactUsPage = ({ cookies }: IndexProps) => (
                 </Button>
               </Stack>
             </form>
-            <iframe
-              style={{ width: "100%", maxWidth: "64em" }}
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.035164897436!2d-93.0972797!3d16.774926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ed275f64ed8311%3A0x4849b474e8b33dfe!2sCircunvalaci%C3%B3n%20238%2C%20Renovaci%C3%B3n%2C%2029010%20Tuxtla%20Guti%C3%A9rrez%2C%20Chis.!5e0!3m2!1ses-419!2smx!4v1656307304532!5m2!1ses-419!2smx"
-            />
           </Stack>
         </Box>
       </VStack>
