25cf2981c6aca1708f3db7f094eb3aef8d80be23
Daniel Campos <37324092+giusniyyel@users.noreply.github.com>
2022-07-02 18:04:27 -0500

    :sparkles: feat(footer): update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,72 @@ import {
   Stack,
   Text,
   Flex,
+  Link,
+  TextProps,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import { NextChakraLink } from "./NextChakraLink";
+
+const ListHeader = ({ children }: TextProps) => {
+  return (
+    <Text fontWeight={"600"} color={"white"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
 
 export const Footer = () => {
   let actualYear = new Date().getFullYear();
 
   return (
     <>
-      <Flex as="footer" bgColor="#1E3760" py={2} position="sticky" mt={8}>
+      <Flex
+        as="footer"
+        bgColor="#1E3760"
+        py={2}
+        position="sticky"
+        mt={8}
+        flexDirection="column"
+        textColor="white"
+      >
+        <Container maxW="container.xl">
+          <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+            <Stack align={"flex-start"}>
+              <ListHeader>Contáctanos</ListHeader>
+              <Link href={"#"}>Overview</Link>
+              <Stack direction={"row"} align={"center"} spacing={2}>
+                <Link href={"#"}>Features</Link>
+              </Stack>
+              <Link href={"#"}>Tutorials</Link>
+              <Link href={"#"}>Pricing</Link>
+              <Link href={"#"}>Releases</Link>
+            </Stack>
+            <Stack align={"flex-start"}>
+              <ListHeader>Company</ListHeader>
+              <Link href={"#"}>About Us</Link>
+              <Link href={"#"}>Press</Link>
+              <Link href={"#"}>Careers</Link>
+              <Link href={"#"}>Contact Us</Link>
+              <Link href={"#"}>Partners</Link>
+            </Stack>
+            <Stack align={"flex-start"}>
+              <ListHeader>Legal</ListHeader>
+              <Link href={"#"}>Cookies Policy</Link>
+              <Link href={"#"}>Privacy Policy</Link>
+              <Link href={"#"}>Terms of Service</Link>
+              <Link href={"#"}>Law Enforcement</Link>
+              <Link href={"#"}>Status</Link>
+            </Stack>
+            <Stack align={"flex-start"}>
+              <ListHeader>Follow Us</ListHeader>
+              <Link href={"#"}>Facebook</Link>
+              <Link href={"#"}>Twitter</Link>
+              <Link href={"#"}>Dribbble</Link>
+              <Link href={"#"}>Instagram</Link>
+              <Link href={"#"}>LinkedIn</Link>
+            </Stack>
+          </SimpleGrid>
+        </Container>
         <Container p={4} maxW="container.xl">
           <Stack
             display="flex"
@@ -20,11 +77,11 @@ export const Footer = () => {
             direction={{ base: "column", md: "row" }}
             width={{ base: "full", md: "auto" }}
           >
-            <Text color="white" align="center">
+            <Text align="center">
               Copyright © {actualYear} Bauztel Café. Todos los derechos
               reservados.
             </Text>
-            <Text color="white" align="center">
+            <Text align="center">
               Powered by{" "}
               <NextChakraLink href="https://www.giusniyyel.com">
                 GiusNiyyel
