cbe9105548e87234fb617b2163946808944d5650
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-07-07 22:50:42 -0500

    :sparkles: feat(module): update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import { Layout } from "../components/Layout/Main";
-
+import Hero from '#components/Sections/Hero'
 const IndexPage = () => (
     <Layout title="Inicio - Bauztel">
+        <section className='container'>
+            <main>
+                <Hero/>
+            </main>
+
+        </section>
     </Layout>
 );
 
