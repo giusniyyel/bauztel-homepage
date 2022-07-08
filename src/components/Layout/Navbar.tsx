118ad802c553acf77dc99f2ee90d48f8927c766d
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-07-07 22:49:37 -0500

    :sparkles: feat(components): add Navbar and Hero

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,6 +1,12 @@
 import { useRouter } from "next/router";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { Logo } from "../Utils/Logo";
+import Link from 'next/link';
+import styles from './Navbar.module.scss'
+import { Bauztel } from "../../shared/data/Logos";
+import { CartIcon } from "../../shared/data/Logos";
+import { UserIcon } from "../../shared/data/Logos";
+
 
 type LinkItemProps = {
   children?: React.ReactNode;
@@ -14,15 +20,43 @@ const LinkItem = (props: LinkItemProps) => {
   const inactiveColor = "#1E3760";
 };
 
-const navLinks = [
-  { title: "Inicio", path: "/" },
-  { title: "Nuestro Café", path: "/our-coffee" },
-  { title: "Nosotros", path: "/about" },
-  { title: "Contáctanos", path: "/contact" },
-];
 
+const navLinks = {
+  '/': 'Inicio',
+  '/about-us': 'Nosotros',
+  '/contact': 'Contactanos'
+}
+
+const NavLink = ({path}: {path:string})=> {
+  const title = navLinks[path];
+  return (
+    <Link href={path}>
+    <a className={styles.link}>{title}</a>
+    </Link>
+  )
+}
 export const Navbar = (props: any) => {
   const { asPath } = useRouter();
 
-  return <></>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Bauztel/>
+      </div>
+      <nav className={styles.navbar}>
+        <ul className={styles.list}>
+          <NavLink path='/'/>
+          <NavLink path="/about-us"/>
+          <NavLink path="/contact"/>
+        </ul>
+        <button>Vender ya!</button>
+        <span className={styles.cart}>
+          <CartIcon/>
+        </span>
+        <span className={styles.cart}>
+          <UserIcon/>
+        </span>
+      </nav>
+    </header>
+  )
 };
---
+++ src/components/Sections/Hero.tsx
@@ -0,0 +1,44 @@
+import styles from './Hero.module.scss'
+import Image from 'next/image'
+const Hero = ()=> {
+    return (
+        <section className={styles.hero}>
+            <div className={styles.wrapper}>
+                <div className={styles.info}>
+                    <h1 className={styles.title}>
+                        Descubre café Bauztel
+                    </h1>
+                    <p className={styles.description}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit voluptas fugit aliquid quos rerum nam officia, dolor temporibus ipsam. Cumque nemo ad commodi enim rem animi quasi minima sapiente, sint 
+                    </p>
+                    <button className={styles.button}>Comprar ya!</button>
+                </div>
+                <Image
+                    src='/hero.png'
+                    height={500}
+                    width={300}
+                    objectFit='contain'
+                    layout='fixed'
+                    className={styles.image}
+                />
+            </div>
+            <ul className={styles.features}>
+                <Feature count={10} name='Molido'/>
+                <Feature count={10} name='Gourmet'/>
+                <Feature count={10} name='Tostado'/>
+            </ul>
+        </section>
+    )
+}
+
+const Feature = ({count, name}:any)=>(
+    <li className={styles.item}>
+    <span className={styles.count}>
+        {count}
+    </span>
+    <span className={styles.name}>
+        {name}
+    </span>
+</li>
+)
+export default Hero
