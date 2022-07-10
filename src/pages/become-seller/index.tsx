c2c510f31a434d1f10147085374ec7e4f28083de
giusniyyel <giusniyyel@gmail.com>
2022-07-10 15:55:56 -0500

    :wrench: chore(pages): update become seller page without style

--- src/pages/become-seller/index.tsx
+++ src/pages/become-seller/index.tsx
@@ -1,5 +1,22 @@
 import { Layout } from '../../components/Layout/Main'
 
-const BecomeSellerPage = () => <Layout title='Inicio - Bauztel'></Layout>
+const BecomeSellerPage = () => (
+  <Layout title='Vender café - Bauztel'>
+    <form>
+      <div>
+        <label>
+          Nombre completo
+          <input type='text' name='full_name' placeholder='Nombre completo' />
+        </label>
+      </div>
+      <div>
+        <label>
+          Correo Electrónico
+          <input type='email' name='email' placeholder='email@example.com' />
+        </label>
+      </div>
+    </form>
+  </Layout>
+)
 
 export default BecomeSellerPage
