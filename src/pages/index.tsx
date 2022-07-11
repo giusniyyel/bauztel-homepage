d3d1a9143d4801c680c8563dbf6c017dd52e8cbf
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-07-10 23:54:43 -0500

    :wrench: chore: add find us section in index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { Layout } from '../components/Layout/Main'
 import Hero from '#components/Sections/Hero'
+import Products from '#components/Sections/Products'
+import FindUs from '#components/Sections/FindUs'
+
 const IndexPage = () => (
   <Layout title='Inicio - Bauztel'>
     <section className='container'>
       <Hero />
+      <Products />
+      <FindUs />
     </section>
   </Layout>
 )
