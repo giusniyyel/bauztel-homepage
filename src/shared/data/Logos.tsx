1444b928037e3aeb54a190320e98cba614b8e10f
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-07-14 21:21:59 -0500

    :wrench: chore: add menu logo for navbar

--- src/shared/data/Logos.tsx
+++ src/shared/data/Logos.tsx
@@ -353,6 +353,9 @@ export const PeopleIcon = () => (
     />
   </svg>
 )
+export const MenuIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="48" d="M88 152h336M88 256h336M88 360h336"/></svg>
+)
 
 // SOCIAL ICONS
 
