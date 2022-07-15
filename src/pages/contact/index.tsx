c8e778ab4724c26bb41842e2d1da4fe311a97fe5
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-07-14 20:58:26 -0500

    :recycle: refactor(pages): update contact page layout

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,59 +1,10 @@
 import { Layout } from '../../components/Layout/Main'
+import ContactContainer from '#components/Containers/ContactContainer'
 
 const ContactUsPage = () => (
   <Layout title='Contáctanos - Bauztel'>
-    <section>
-      <h1>Contáctanos</h1>
-      <p>
-        En Bauztel Café queremos conocer tus opiniones, comentarios, o el
-        interés que tienes sobre nuestros productos.
-      </p>
-    </section>
-    <section>
-      <div>
-        <p>Escríbenos</p>
-        <p>Deja tu mensaje para que podamos contactarte a la brevedad.</p>
-        <form id='contact'>
-          <div>
-            <label>
-              Nombre Completo
-              <input type='text' name='name' placeholder='Nombre completo' />
-            </label>
-          </div>
-          <div>
-            <label>
-              Correo Electrónico
-              <input
-                type='email'
-                name='email'
-                placeholder='contact@bauztel.com'
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Mensaje
-              <input
-                type='email'
-                name='email'
-                placeholder='Estoy interesado en conocer acerca de...'
-              />
-            </label>
-          </div>
-          <input type='submit' value='Enviar' />
-        </form>
-      </div>
-      <div>
-        <div>{/* Mapa o Imagen */}</div>
-        <div>
-          <p>Síguenos en...</p>
-          <div>
-            <div>{/* facebook icon */}</div>
-            <div>{/* instagram icon */}</div>
-            <div>{/* whatsapp icon */}</div>
-          </div>
-        </div>
-      </div>
+    <section className="container">
+      <ContactContainer/>
     </section>
   </Layout>
 )
